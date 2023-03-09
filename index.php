20e062ba587ddaf833fd5b4ec3cdfc0986cc3350
paszosky <pavel.kowalczyk@gmail.com>
2023-03-09 15:47:08 +0100

    functions

--- index.php
+++ index.php
@@ -58,8 +58,54 @@
                 'director' => 'Robert Zemeckis',
                 'year' => 1994,
                 'link' => 'https://www.filmweb.pl/film/Forrest+Gump-1994-998'
+            ],
+            [
+                'name' => 'Back to the Future',
+                'director' => 'Robert Zemeckis',
+                'year' => 1985,
+                'link' => 'https://www.filmweb.pl/film/Powr%C3%B3t+do+przysz%C5%82o%C5%9Bci-1985-8823'
             ]
-        ]
+            ];
+
+            function filterByAuthor($films, $director) {
+                $filteredFilms = [];
+
+                foreach ($films as $film) {
+                    if ($film['director'] == $director) {
+                        $filteredFilms[] = $film;
+                    }
+                }
+
+                return $filteredFilms;
+            }
+
+            function filterByYear($films, $year) {
+                $filteredFilms = [];
+
+                foreach ($films as $film) {
+                    if ($film['year'] >= $year) {
+                        $filteredFilms[] = $film;
+                    } 
+                }
+                return $filteredFilms;
+            }
+
+            function filter($items, $fn)
+            {
+                $filteredItems = [];
+
+                foreach ($items as $item) {
+                    if ($fn($item)) {
+                        $filteredItems[] = $item;
+                    }
+                }
+
+                return $filteredItems;
+            }
+
+            $filteredFilmsNew = filter($films, function($film) {
+                return $film['director'] == 'Robert Zemeckis';
+            });
     ?>
 
     <p>Recommended books</p>
@@ -79,12 +125,38 @@
 
     <ul>Filmy
     <?php foreach ($films as $film) : ?>
+        <?php if ($film['director'] == 'Robert Zemeckis') : ?>
         <li>
             <a href="<?= $film['link']?>">    
                 <?= $film['name'] . " | " . $film['year'] ?>
             </a>
         </li>
+        <?php endif ?>
     <?php endforeach; ?>
     </ul>
+    
+
+    <h5>Filtered film</h5>
+    <?php foreach (filterByAuthor($films, 'Frank Darabont') as $film) : ?>
+        <li>
+            <a href="#">
+                <?= $film['name']; ?> - <?= $film['director']; ?>
+            </a>
+        </li>
+    <?php endforeach; ?>
+
+    <h5>Filtered by year</h5>
+    <?php foreach (filterByYear($films, 1990) as $film) : ?>
+        <li>
+            <?= $film['name']; ?> - <?= $film['year'] ?>
+        </li>
+    <?php endforeach; ?>
+
+    <h5>Filtered new</h5>
+    <?php foreach ($filteredFilmsNew as $film) : ?>
+        <li>
+            <?= $film['name']; ?> - <?= $film['year'] ?>
+        </li>
+    <?php endforeach; ?>
 </body>
 </html>
