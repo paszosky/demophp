1c87bff6c042307935cea6f3cf408606762d5030
paszosky <pavel.kowalczyk@gmail.com>
2023-03-15 11:54:25 +0100

    connect to database

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
          
 // dd($_SERVER);
 
+// connect to database
+$dsn = "mysql:host=localhost;port=3306;user=root;dbname=demophp;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts where id=1");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
